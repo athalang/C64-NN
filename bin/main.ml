f6fa813b676ac928d8d26dbbceeeb238a3f80f4a
athalang <athalang@hotmail.com>
2025-10-09 16:09:18 +0100

    Redo tgz read

--- bin/main.ml
+++ bin/main.ml
@@ -2,32 +2,21 @@ open Core
 
 let tmpdir = Filename_unix.temp_dir "c64nn" ""
 
-let untar_unzip filename =
-  let realpath = Filename_unix.realpath filename in
-  match Tar_unix.extract ~src:realpath tmpdir with
-  | Ok () -> ()
-  | Error `Fatal e ->
-    Tar.pp_error Format.err_formatter e;
-    Out_channel.newline stderr;
-    prerr_endline ("from: " ^ realpath)
-  | Error `Unix (err, _, _) ->
-    prerr_endline ("Unix error: " ^ Core_unix.Error.message err);
-    prerr_endline ("from: " ^ realpath)
-  | Error `Unexpected_end_of_file ->
-    prerr_endline ("Unexpected EOF error: " ^ realpath)
-  | Error `Msg s ->
-    prerr_endline ("Error: " ^ s);
-    prerr_endline ("from: " ^ realpath)
-  | Error `Exn _ ->
-    prerr_endline ("Exception when extracting NNEF from: " ^ realpath)
+let un_tgz archive dst =
+  let path = Filename_unix.realpath archive in
+  let proc = Core_unix.create_process
+    ~prog:"tar"
+    ~args:["-zxf"; path; "-C"; dst]
+  in
+  Core_unix.waitpid proc.pid
+  |> Core_unix.Exit_or_signal.or_error
+  |> Or_error.ok_exn
 
 let command =
   Command.basic
     ~summary:"Compile an NNEF gzipped tarball into a C64 memory image"
-    (Command.Param.(
-      map
-        (anon ("filename" %: string))
-        ~f:(fun filename () ->
-          untar_unzip filename)))
+    Command.Let_syntax.(
+      let%map_open archive = anon ("ARCHIVE.TGZ" %: string) in
+      fun () -> un_tgz archive tmpdir)
 
 let () = Command_unix.run command
